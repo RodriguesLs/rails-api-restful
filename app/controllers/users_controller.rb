c657eb7148a69da51ecc2731cd7f6b52d03c8906
Rodrigues <fr.costa95@gmail.com>
2021-02-04 07:11:26 -0300

    feat(controller, model): include validation and all actions
    	Include all actions for Restful API using varbs and respective
    behaviors.
    	Include validation presence of username on UserModel.
    
    	Finish practice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(update show destroy)
+  
   def index
     render json: User.all, status: :ok
   end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }
+    end
+  end
+
+  def update
+    @user.update(user_params)
+
+    if @user.save
+      render json: @user, status: :ok
+    else
+      render json: { errors: user.erros.full_messages } 
+    end
+  end
+
+  def show
+    render json: @user
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+  
+    def set_user
+      @user ||= User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
